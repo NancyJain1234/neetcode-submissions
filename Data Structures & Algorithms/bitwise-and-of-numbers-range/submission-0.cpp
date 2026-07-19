8be9e23fc0f9b7f0482cdd09876747127cb8e024
NancyJain1234 <nancyagrawal55758@gmail.com>
2026-07-19 11:59:25 +0530

    Add: bitwise-and-of-numbers-range - submission-0

---
+++ Data Structures & Algorithms/bitwise-and-of-numbers-range/submission-0.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int res = left ;
+        for(int i = left + 1 ; i <= right ; i++) res = (res & i);
+        return res;
+    }
+};
